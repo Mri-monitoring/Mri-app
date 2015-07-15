e017919e7b84920479c0872a50f72774bcdaa4bb
Nate Harada <nharada1@gmail.com>
2015-07-15 14:44:36 -0400

    Renamed MriClient to MriCaffe

--- Mri/MriApp.py
+++ Mri/MriApp.py
@@ -6,7 +6,7 @@ from __future__ import absolute_import
 from future import standard_library
 standard_library.install_aliases()
 
-from Mri.MriClient import MriClient
+from Mri.MriCaffe import MriClient
 
 import argparse
 
